23dbe5a6994c1346bd4504f02d8e53d734d380ac
divine <divineobi07@gmail.com>
2023-09-07 21:05:17 +0100

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+const day = document.querySelector(".day");
+const time = document.querySelector(".time");
+const currentTime = new Date();
+
+const dayArr = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+day.textContent = dayArr[currentTime.getDay()];
+time.textContent = +currentTime
+
+console.log(+currentTime);
